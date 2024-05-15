c43fac670e7e523abdd9db4f9004f2dcccf4c2c6
Pujan Bohora <bohorapawan@gmail.com>
2024-05-15 20:35:30 +0545

    Initial Commit

---
+++ CurrencyCalculator/CurrencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCalculatorApp.swift
+//  CurrencyCalculator
+//
+//  Created by Pawan Bohora on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
